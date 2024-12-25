7d0ddc37273cdd8fd51a723c0a9c3d5863421e5e
kannadakali <pranav.gunhal@gmail.com>
2024-12-25 11:55:43 -0800

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -5,11 +5,12 @@ const jwt = require("jsonwebtoken")
 module.exports.userVerification = (req, res) => {
     const token = req.cookies.token 
     if(!token) {
-        console.log("Token verification failed:", err.message);
+        console.log("no token found")
         return res.json({ status: false })
     }
     jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
         if (err) {
+            console.log("verification falied", err.message)
             return res.json({ status: false })
         } 
 
